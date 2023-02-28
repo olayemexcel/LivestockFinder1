b5610c9aceb8c0aa63d3f158af46184af4b90bba
olayemexcel <35165903+olayemexcel@users.noreply.github.com>
2023-02-28 18:07:17 +0100

    Login validation and Debugging crashes when login successful

--- app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/Login.java
+++ app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/Login.java
@@ -1,29 +1,52 @@
 package uk.ac.tees.b1210259.livestockfinder1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
 
-    Button btn1;
+    Button btnSignUp, btnLogin;
+    ImageView image;
+    TextView logoText, sloganText;
+    TextInputLayout username, password;
+
+    //Database reference
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
 
-        btn1 = findViewById(R.id.button3);
 
-        btn1.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.button2);
+        btnSignUp = findViewById(R.id.button3);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // Intent intent = new Intent(Login.this,Signup);
                 Intent intent = new Intent(getApplicationContext(), Signup.class);
                 startActivity(intent);
 
@@ -31,4 +54,98 @@ public class Login extends AppCompatActivity {
         });
 
     }
+
+
+    private Boolean validateUsername() {
+        String val = username.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            username.setError("Field cannot be empty");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view) {
+
+        //Login Validation
+        if (!validateUsername() | !validatePassword()) {
+            return;
+        } else {
+            isUser();
+        }
+    }
+
+    private void isUser() {
+        String userEnteredUsername = username.getEditText().getText().toString().trim();
+        String userEnteredPassword = password.getEditText().getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+
+                    Log.v("Login", userEnteredUsername + "," + userEnteredPassword);
+                    String passwordFromDB = snapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+
+                    if (passwordFromDB.equals(userEnteredPassword)) {
+
+
+                        password.setError(null);
+                        password.setErrorEnabled(false);
+
+                        String nameFromDB = snapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                        String usernameFromDB = snapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                        String phoneNoFromDB = snapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
+                        String emailFromDB = snapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+                        Intent intent = new Intent(getApplicationContext(), UserProfile.class);
+
+                        intent.putExtra("name", nameFromDB);
+                        intent.putExtra("username", usernameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("phoneNo", phoneNoFromDB);
+                        intent.putExtra("password", passwordFromDB);
+
+                        startActivity(intent);
+                    } else {
+                        password.setError("Invalid Password");
+                        password.requestFocus();
+                    }
+                } else {
+                    username.setError("Invalid Username");
+                    username.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/Signup.java
+++ app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/Signup.java
@@ -32,7 +32,7 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //disable status top bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_signup);
 
 
@@ -46,10 +46,8 @@ public class Signup extends AppCompatActivity {
         regLoginBtn = findViewById(R.id.button4);
 
 
-
-
         //Click button to save data in Firebase
-        regBtn.setOnClickListener(new View.OnClickListener() {
+       /* regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 rootNode = FirebaseDatabase.getInstance();
@@ -65,11 +63,7 @@ public class Signup extends AppCompatActivity {
                 reference.child(phoneNo).setValue(helperClass);
                 Toast.makeText(Signup.this, "Saving data..", Toast.LENGTH_SHORT).show();
             }
-        }); //Signup button method End
-
-
-
-
+        }); *///Signup button method End
 
 
         //onclick event to another page
@@ -81,5 +75,111 @@ public class Signup extends AppCompatActivity {
                 startActivity(intent);
             }
         });// Login button method end
-    } // OnCreate methods end
+    } // OnCreate methods end Old signup
+
+
+    private Boolean validateName() {
+        String val = regName.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            regName.setError("Field cannot be empty");
+            return false;
+        } else {
+            regName.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateUsername() {
+        String val = regUsername.getEditText().getText().toString();
+        String noWhiteSpace = "\\A\\w{4,20}\\z";
+        if (val.isEmpty()) {
+            regUsername.setError("Field cannot be empty");
+            return false;
+        } else if (val.length() >= 15) {
+            regUsername.setError("Username too long");
+            return false;
+        } else if (!val.matches(noWhiteSpace)) {
+            regUsername.setError("White Spaces are not allowed");
+            return false;
+        } else {
+            regUsername.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail() {
+        String val = regEmail.getEditText().getText().toString();
+        String emailFormat = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            regEmail.setError("Field cannot be empty");
+            return false;
+        } else if (!val.matches(emailFormat)) {
+            regEmail.setError("Invalid email address");
+            return false;
+        } else {
+            regEmail.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validatePhoneNo() {
+        String val = regPhoneNo.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            regPhoneNo.setError("Field cannot be empty");
+            return false;
+        } else {
+            regPhoneNo.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        String val = regPassword.getEditText().getText().toString();
+        /*String passwordVal = "^" +
+                "(?=.*[a-zA-Z])" +  //ant letter
+                "(?=.*[@#$%^&+=])" +  //at least 1 special character
+                "(?=\\s+$)" +  //no white space
+                ".{4,}" +  //at least 4 char
+                "$";  */
+
+        if (val.isEmpty()) {
+            regPassword.setError("Field cannot be empty");
+            return false;
+        } /*else if (!val.matches(passwordVal)) {
+            regPassword.setError("Password is too weak");
+            return false;
+        }*/ else {
+            regPassword.setError(null);
+            return true;
+        }
+    }
+
+
+    //regBtn.setOnClickListener(new View.OnClickListener() {
+    // @Override
+    //Save data in firebase when click button
+    public void registerUser(View view) {
+
+       rootNode = FirebaseDatabase.getInstance();
+       reference = rootNode.getReference("users");
+
+
+        if (!validateName() | !validatePassword() | !validatePhoneNo() | !validateEmail() | !validateUsername()) {
+            return;
+        }
+
+        //Get all the values in String
+        String name = regName.getEditText().getText().toString();
+        String username = regUsername.getEditText().getText().toString();
+        String email = regEmail.getEditText().getText().toString();
+        String phoneNo = regPhoneNo.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+        UserSignupClass helperClass = new UserSignupClass(name, username, email, phoneNo, password);
+        reference.child(username).setValue(helperClass);
+        Toast.makeText(Signup.this, "Data Save Successfully!", Toast.LENGTH_SHORT).show();
+
+    }
+
 }
+
