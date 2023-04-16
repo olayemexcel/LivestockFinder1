c1ed370e87243e88c3214f08f3febd01804994d0
olayemexcel <35165903+olayemexcel@users.noreply.github.com>
2023-04-16 17:24:01 +0100

    Google places API added for autocomplete address.

--- app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/UserProfile.java
+++ app/src/main/java/uk/ac/tees/b1210259/livestockfinder1/UserProfile.java
@@ -5,18 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
 
     Button btnExplore;
     TextView fullNameLabel, usernameLabel;
     TextInputLayout fullName,email,phoneNo,password;
+
+    //initialize location variables
+    EditText editText;
+    TextView textView;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,6 +41,12 @@ public class UserProfile extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+
+        //assign location variable
+        editText = findViewById(R.id.edit_location);
+        //textView = findViewById(R.id.view_address);
+
 
         //Hooks
         fullName = findViewById(R.id.prof_full_name);
@@ -51,7 +73,50 @@ public class UserProfile extends AppCompatActivity {
         }); //Button explore ends
 
 
+        //initialize places
+        Places.initialize(getApplicationContext(), "AIzaSyA_ReHuBhQwXPh7AVdO2hjYzVQ1VVI9kNE");
+
+        //set edit text non focusable
+        editText.setFocusable(false);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //initialize place field list
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS,Place.Field.LAT_LNG,Place.Field.NAME);
+                //Create intent
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(UserProfile.this);
+                //start activity result
+
+                startActivityForResult(intent,100);
+            }
+        }); // end
+
+
     }
+
+    //continuation of google places code
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            //when success
+
+            //initialize place
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            //Set address on EditText
+            editText.setText(String.format("Postcode : %s",place.getAddress()));
+        } else if (resultCode == AutocompleteActivity.RESULT_ERROR){
+            //when error
+            //initialize status
+            Status status = Autocomplete.getStatusFromIntent(data);
+            //Display toast
+            Toast.makeText(getApplicationContext(),status.getStatusMessage(),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
     private void showAllUserData() {
         Intent intent = getIntent();
         String user_username = intent.getStringExtra("username");
